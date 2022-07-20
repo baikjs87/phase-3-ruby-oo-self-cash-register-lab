8b71cc4b76c488a6fcae1b88dd7b56747b4069fa
Jason Baik <baikjs87@gmail.com>
2022-07-20 02:30:30 -0400

    Stuck on CashRegister

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require "pry"
 
+class CashRegister
+    attr_reader :total, :discount
+
+    def initialize total = 0, discount = 20
+        @total = total
+        @discount = discount
+    end
+
+    def total=value
+        @total = value
+    end
+
+    def add_item title, price, quantity = 1
+        @total += (price * quantity)
+    end
+
+    def apply_discount
+        @total = (@total - @discount) - (@total - @discount) * (@discount/100.0)
+        "After the discount, the total comes to $#{total.to_i}."
+    end
+
+end
